f79f9d5911821cf841d68a051d1fae974e494519
Vitaly Cloud <vitalycloud@gmail.com>
2022-12-16 04:45:51 +0300

    tvOS Support

--- init-ios.sh
+++ init-ios.sh
@@ -44,6 +44,7 @@ FF_ALL_ARCHS_IOS7_SDK="armv7 armv7s arm64 i386 x86_64"
 FF_ALL_ARCHS_IOS8_SDK="armv7 arm64 i386 x86_64"
 FF_ALL_ARCHS=$FF_ALL_ARCHS_IOS8_SDK
 FF_TARGET=$1
+FF_PATH=$2
 
 function echo_ffmpeg_version() {
     echo $IJK_FFMPEG_COMMIT
@@ -60,8 +61,8 @@ function pull_common() {
 
 function pull_fork() {
     echo "== pull ffmpeg fork $1 =="
-    sh $TOOLS/pull-repo-ref.sh $IJK_FFMPEG_FORK ios/ffmpeg-$1 ${IJK_FFMPEG_LOCAL_REPO}
-    cd ios/ffmpeg-$1
+    sh $TOOLS/pull-repo-ref.sh $IJK_FFMPEG_FORK $FF_PATH/ffmpeg-$1 ${IJK_FFMPEG_LOCAL_REPO}
+    cd $FF_PATH/ffmpeg-$1
     git checkout ${IJK_FFMPEG_COMMIT} -B ijkplayer
     cd -
 }
@@ -74,7 +75,7 @@ function pull_fork_all() {
 }
 
 function sync_ff_version() {
-    sed -i '' "s/static const char \*kIJKFFRequiredFFmpegVersion\ \=\ .*/static const char *kIJKFFRequiredFFmpegVersion = \"${IJK_FFMPEG_COMMIT}\";/g" ios/IJKMediaPlayer/IJKMediaPlayer/IJKFFMoviePlayerController.m
+    sed -i '' "s/static const char \*kIJKFFRequiredFFmpegVersion\ \=\ .*/static const char *kIJKFFRequiredFFmpegVersion = \"${IJK_FFMPEG_COMMIT}\";/g" $FF_PATH/IJKMediaPlayer/IJKMediaPlayer/IJKFFMoviePlayerController.m
 }
 
 #----------
